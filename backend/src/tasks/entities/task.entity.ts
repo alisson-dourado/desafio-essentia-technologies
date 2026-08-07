bf56c2ef6736fa69a0656779ac6d529434225f71
Alisson Dourado <alissondouradodev@gmail.com>
2026-08-06 23:44:29 -0300

    feat: add task entity

---
+++ backend/src/tasks/entities/task.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ length: 255 })
+  title!: string;
+
+  @Column({ type: 'text', nullable: true })
+  description!: string | null;
+
+  @Column({ default: false })
+  completed!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
